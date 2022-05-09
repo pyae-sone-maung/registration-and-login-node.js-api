4da18d1f65fb5e7be80cad53fde728c9de1495fa
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-05-09 23:16:01 +0630

    create user model

---
+++ src/model/user-model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+    {
+        name: { type: String },
+        email: { type: String, unique: true },
+        date_of_birth: { type: Date },
+        gender: { type: String },
+        phone: { type: String },
+        address: { type: String },
+        password: { type: String },
+        confirm_otp: { type: String },
+        isVerified: { type: Boolean, default: false },
+    },
+    { versionKey: false },
+    { timestamp: true }
+);
+
+module.exports = mongoose.model("user", userSchema);
