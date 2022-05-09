ef0f22c9e1800b3f90f4c209af2d695114bfc214
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-05-09 23:18:05 +0630

    random number function

---
+++ src/utils/otp-generator.js
@@ -0,0 +1,8 @@
+const randomNumber = (length) => {
+    const otp = Math.random()
+        .toString()
+        .slice(2, parseInt(length + 2));
+    return otp;
+};
+
+module.exports = { randomNumber };
