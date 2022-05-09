99c6f63d4b640cef4fb76ea490e606f527f28476
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-05-09 23:18:29 +0630

    check user req

---
+++ src/validation/user-validation.js
@@ -0,0 +1,23 @@
+const { check } = require("express-validator");
+
+const userValidation = [
+    check("name").not().isEmpty().trim().withMessage("Name is required."),
+    check("email").not().isEmpty().trim().withMessage("Email is required."),
+    check("date_of_birth")
+        .not()
+        .isEmpty()
+        .isDate()
+        .trim()
+        .withMessage("Date of birth is required."),
+    check("gender").not().isEmpty().trim().withMessage("Gender is required."),
+    check("phone").not().isEmpty().trim().withMessage("Phone is required."),
+    check("address").not().isEmpty().trim().withMessage("Address is required"),
+    check("password")
+        .not()
+        .isEmpty()
+        .isLength({ min: 8 })
+        .trim()
+        .withMessage("Password is at least 8 character."),
+];
+
+module.exports = { userValidation };
