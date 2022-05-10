fdfb77a3348bbe68813373a24efcbd6d82795424
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-05-10 23:42:45 +0630

    add sent mail function

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,22 +1,24 @@
 const userModel = require("../model/user-model");
 const bcrypt = require("bcrypt");
-const { validationResult } = require("express-validator");
+const { validationResult, check } = require("express-validator");
 const { randomNumber } = require("../utils/otp-generator");
+const { sentMail } = require("../utils/mail-sender");
 
-const createNewUser = (req, res, next) => {
+const createNewUser = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty())
         return res.status(400).json({ Error: errors.array() });
 
-    const checkEmail = userModel.findOne({ email: req.body.email });
-    if (checkEmail)
+    const isEmailExist = await userModel.findOne({ email: req.body.email });
+    if (isEmailExist)
         return res
             .status(400)
             .json({ Error: "Email address is already exist." });
 
-    bcrypt.hash(req.body.password, 10, async (err, hash) => {
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
         if (err) return res.status(500).json({ Error: err.array() });
         try {
+            const otp = randomNumber(6);
             const user = {
                 name: req.body.name,
                 email: req.body.email,
@@ -25,17 +27,20 @@ const createNewUser = (req, res, next) => {
                 phone: req.body.phone,
                 address: req.body.address,
                 password: hash,
-                confirm_otp: randomNumber(6),
+                confirm_otp: otp,
             };
-            const data = await userModel.insertMany(user);
-            return res
-                .status(200)
-                .json({ meta: { message: "create successfully" }, data });
+
+            userModel.insertMany(user, (err, data) => {
+                if (err) return res.status(500).json({ Error: err });
+                sentMail(req.body.email, otp);
+                return res.status(201).json({
+                    message: "verification code sent to your email address.",
+                });
+            });
         } catch (error) {
             return res.status(500).json({ Error: error });
         }
     });
-    console.log("create successfully");
 };
 
 module.exports = {
