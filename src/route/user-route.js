de0783d4b888c9ee4d124ea7be95f5149ee84558
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-05-11 23:49:02 +0630

    add login route

--- src/route/user-route.js
+++ src/route/user-route.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { userValidation } = require("../validation/user-validation");
+const {
+    userValidation,
+    loginValidation,
+} = require("../validation/user-validation");
 const userController = require("../controller/user-controller");
 
 router.post("/register", userValidation, userController.createNewUser);
+
+router.post("/login", loginValidation, userController.userLogin);
 
 module.exports = router;
