b799c165fed17a061dee23f73d26f3dfb20b800d
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-05-11 23:48:14 +0630

    modify mail sending

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -2,7 +2,7 @@ const userModel = require("../model/user-model");
 const bcrypt = require("bcrypt");
 const { validationResult, check } = require("express-validator");
 const { randomNumber } = require("../utils/otp-generator");
-const { sentMail } = require("../utils/mail-sender");
+const { mailSender } = require("../utils/mail-sender");
 
 const createNewUser = async (req, res, next) => {
     const errors = validationResult(req);
@@ -32,7 +32,15 @@ const createNewUser = async (req, res, next) => {
 
             userModel.insertMany(user, (err, data) => {
                 if (err) return res.status(500).json({ Error: err });
-                sentMail(req.body.email, otp);
+
+                const html =
+                    "<p> Dear " +
+                    data[0].name +
+                    ", </p> </br> </br> <p> Please find below the verification code to complete your registration. </br> </br> <h3> Verification Code: " +
+                    data[0].confirm_otp +
+                    " </h3> </br> </br> <p> Please feel free to contact us if you have any question.Thank you.</p> </br> <p> Best Regards,</p>";
+
+                mailSender(req.body.email, html, otp);
                 return res.status(201).json({
                     message: "verification code sent to your email address.",
                 });
@@ -43,6 +51,36 @@ const createNewUser = async (req, res, next) => {
     });
 };
 
+const userLogin = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+        return res.status(400).json({ Error: errors.array() });
+
+    const { email, password } = req.body;
+    try {
+        const data = await userModel.findOne({ email: email });
+        if (data === null) return res.sendStatus(404);
+        if (data.isVerified === false)
+            return res
+                .status(401)
+                .json({ message: "Your account is not verified." });
+
+        bcrypt.compare(password, data.password, (err, result) => {
+            if (result === false)
+                return res
+                    .status(401)
+                    .json({ Error: "email and password are not match." });
+            return res.status(200).json({
+                meta: { message: "login successfully" },
+                data,
+            });
+        });
+    } catch (error) {
+        return res.status(500).json({ Error: error });
+    }
+};
+
 module.exports = {
     createNewUser,
+    userLogin,
 };
