fce4b71d52decf019cad89de677f8d05a8106a9a
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-05-14 23:34:42 +0630

    add new routes

--- src/route/user-route.js
+++ src/route/user-route.js
@@ -3,11 +3,25 @@ const router = express.Router();
 const {
     userValidation,
     loginValidation,
+    accountVerifyValidation,
+    resetPasswordValidation,
 } = require("../validation/user-validation");
 const userController = require("../controller/user-controller");
 
 router.post("/register", userValidation, userController.createNewUser);
 
 router.post("/login", loginValidation, userController.userLogin);
+
+router.post("/verify", accountVerifyValidation, userController.accountVerify);
+
+router.post("/resend-verify", userController.resendConfirmOTP);
+
+router.post("/forgot-password", userController.forgotPassword);
+
+router.patch(
+    "/reset-password",
+    resetPasswordValidation,
+    userController.resetPassword
+);
 
 module.exports = router;
