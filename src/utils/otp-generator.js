6cc0ee4fa80847f9aa787b72235d2521ac52a5a7
pyae-sone-maung <pyaesonemaung.psm1996@gmail.com>
2022-05-14 23:37:01 +0630

    modify OTP fun()

--- src/utils/otp-generator.js
+++ src/utils/otp-generator.js
@@ -1,8 +1,8 @@
-const randomNumber = (length) => {
+const verifyNumber = (length) => {
     const otp = Math.random()
         .toString()
         .slice(2, parseInt(length + 2));
     return otp;
 };
 
-module.exports = { randomNumber };
+module.exports = { verifyNumber };
